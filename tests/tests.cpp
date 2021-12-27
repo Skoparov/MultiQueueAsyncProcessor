975340272e7e354886af47f5c966a8f9a73d15a2
Sergey Koparov <sergeykoparov@gmail.com>
2021-12-27 22:56:53 +0300

    Add cmake for tests

---
+++ tests/tests.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE MultiQueueAsyncProcessorTests
+
+#include <boost/test/unit_test.hpp>
+
+
+namespace multi_queue_async_processor::tests {
+
+
+}// multi_queue_async_processor::tests
